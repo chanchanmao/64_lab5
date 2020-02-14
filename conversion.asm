dc180cb53c463026595e1f2aa9c0f22fd8e05e8e
ChanChan Mao <chanchanmao@ucsb.edu>
2020-02-14 05:11:58 -0800

    done conversion

--- conversion.asm
+++ conversion.asm
@@ -5,13 +5,14 @@
 #   make all arguments to any function go in $a0 and/or $a1
 #   make all returned values from functions go in $v0
 
+.data
+	nl: .asciiz "\n"
+
 .text
 conv:
     # TODO: Write your function code here
-	addiu $sp, $sp, -12
-	sw $ra, 8($sp)
-	sw $s0, 4($sp)
-	sw $s1, 0($sp)
+	addiu $sp, $sp, -4 # save return address
+	sw $ra, 0($sp)
 
 	move $s2, $a0 # x
 	move $s3, $a1 # y
@@ -22,44 +23,35 @@ conv:
 	j loop
 	
 loop:
-	beq $t0, $t1, return
+	beq $t0, $t1, return # if !(i < 5), exit for loop
 
-	# loop body
+	# for loop body
 	addi $t0, $t0, 1 # increment counter
 	
 	sll $t2, $s2, 1 # 2*x
-	add $s4, $s4, $t2 # z - 2*x
+	add $s4, $s4, $t2 # z + 2*x
 	sub $s4, $s4, $s3 # z - 2*x - y
 
-	jal if
-
-	j loop
+	j if # go to if loop
 
 return:
-	move $v0, $s4
-	lw $ra, 8($sp)
-	lw $s0, 4($sp)
-	lw $s1, 0($sp)
-	addiu $sp, $sp, 12
-
-	jr $ra
-
-if:
-	addiu $sp, $sp, -4
-	sw $ra, 0($sp)
-
-	li $t3, 3 # compare
-	blt $s2, $t3, end_if
-	addi $s3, $s3, -1 # y -= 1
+	move $v0, $s4 # return value
 
 	sw $ra, 0($sp)
 	addiu $sp, $sp, 4
 
-	jr $ra
+	jr $ra # return to main
+
+if:
+	li $t3, 3 # compare
+	blt $s2, $t3, end_if # if !(x >= 3), exit if loop
+	addi $s3, $s3, -1 # y -= 1
+
+	j end_if # exit if loop
 
 end_if:
 	addi $s2, $s2, 1 # x += 1
-	j loop
+	j loop # restart loop
 
 main:
 	# TODO: Write your main function code here
@@ -71,7 +63,7 @@ main:
 	move $a0, $v0 # c
 	li $v0, 1
 	syscall
-
+	
 exit:
 	li $v0, 10
 	syscall

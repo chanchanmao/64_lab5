59b1d0b5c2078f36e505ebfff481c4df483edd13
ChanChan Mao <chanchanmao@ucsb.edu>
2020-02-14 04:45:53 -0800

    output 25?

--- conversion.asm
+++ conversion.asm
@@ -8,53 +8,68 @@
 .text
 conv:
     # TODO: Write your function code here
+	addiu $sp, $sp, -12
+	sw $ra, 8($sp)
+	sw $s0, 4($sp)
+	sw $s1, 0($sp)
+
+	move $s2, $a0 # x
+	move $s3, $a1 # y
+
+	li $s4, 0 # z
+	li $t0, 0 # counter
+	li $t1, 5 # compare
+	j loop
+	
+loop:
+	beq $t0, $t1, return
+
+	# loop body
+	addi $t0, $t0, 1 # increment counter
+	
+	sll $t2, $s2, 1 # 2*x
+	add $s4, $s4, $t2 # z - 2*x
+	sub $s4, $s4, $s3 # z - 2*x - y
+
+	jal if
+
+	j loop
+
+return:
+	move $v0, $s4
+	lw $ra, 8($sp)
+	lw $s0, 4($sp)
+	lw $s1, 0($sp)
+	addiu $sp, $sp, 12
+
+	jr $ra
+
+if:
 	addiu $sp, $sp, -4
 	sw $ra, 0($sp)
 
-	move $a0, $s0 # x
-	move $a1, $s1 # y
+	li $t3, 3 # compare
+	blt $s2, $t3, end_if
+	addi $s3, $s3, -1 # y -= 1
 
-	li $v0, 0 # z
-	li $t0, 0 # i
-	li $t1, 5 # compare with this value
-
-	j loop_for
-
-loop_for:
-	bgt $t0, $t1, exit # if !(i < 5) = if (i >= 5)
-	sll $a0, $a0, 1 # 2 * x
-	add $v0, $v0, $a0 # z + 2x
-	sub $v0, $v0, 1 # z +2x - y
-
-	jal loop_if
-
-	addi $t0, $t0, -1
-
-	j loop_for
-
-loop_if:
-	li $t2, 3 # compare with this value
-	bge $a0, 3, if
-	addi $a0, $a0, 1
-
-if:
-	addi $a1, $a1, -1
-	jr $ra
-
-	lw $ra, 0($sp)
+	sw $ra, 0($sp)
 	addiu $sp, $sp, 4
+
 	jr $ra
+
+end_if:
+	addi $s2, $s2, 1 # x += 1
+	j loop
 
 main:
 	# TODO: Write your main function code here
-	li $s0, 5 # a
-	li $s1, 7 # b
-
+	li $a0, 5 # a
+	li $a1, 7 # b
 	jal conv
 
 	# print
+	move $a0, $v0 # c
 	li $v0, 1
-	move $a0, $v0, # store c in $v0
 	syscall
 
 exit:

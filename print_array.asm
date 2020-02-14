ae77e6dc5bd973d37643522e68f135a292510edd
ChanChan Mao <chanchanmao@ucsb.edu>
2020-02-14 05:46:31 -0800

    arr

--- print_array.asm
+++ print_array.asm
@@ -10,17 +10,16 @@
 	arr: .word 1 2 3 4 5 6 7 8 9 10
 	len: .word 10
 	newline: .asciiz "\n"
-	content: .asciiz "The contents of the array are: \n"
+	content: .asciiz "The contents of the array are:\n"
 
 .text
 printA:
-    # TODO: Write your function code here
 	la $t1, len # get address of array length
 	lw $t2, 0($t1) # store value of array length
 
 	bge $t0, $t2, exit # if !(x < array length) = if (x >= array length)
 
-	la $t4, arr # else...
+	la $t4, arr # get base of array
 	sll $t5, $t0, 2
 	add $t6, $t5, $t4
 	lw $t7, 0($t6)
@@ -38,9 +37,9 @@ printA:
 	j printA # restart loop
 
 main:
-	# TODO: Write your main function code here
-	li $t0, 0
+	li $t0, 0 # index
 
+	# print
 	li $v0, 4
 	la $a0, content
 	syscall
